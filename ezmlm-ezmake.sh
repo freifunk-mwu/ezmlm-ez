b5bdee199c5fe8e9249cfc230d7646f48ea0cb39
Frieder Grießhammer <frieder.griesshammer@der-beweis.de>
2014-08-31 18:49:05 +0200

    Add server dependant settings

--- ezmlm-ezmake.sh
+++ ezmlm-ezmake.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-LDIR="$HOME/ezmlm"
-DOMAIN="freifunk-rheinmain.de"
+case $(whoami) in
+    freifunk)
+        LDIR="$HOME/listen"
+        DOMAIN="freifunk-mainz.de"
+        ;;
+    ffwi)
+        LDIR="$HOME/ezmlm"
+        DOMAIN="freifunk-wiesbaden.de"
+        ;;
+    ffrm)
+        LDIR="$HOME/ezmlm"
+        DOMAIN="freifunk-rheinmain.de"
+        ;;
+    *)
+        echo "Error! - No settings found"
+        ;;
+esac
+
+if [ -z "$LDIR" ] || [ -z "$DOMAIN" ]; then
+    exit 23
+fi
+
 
 function public_list()
 {
@@ -25,10 +45,11 @@ while test $# -gt 0; do
     case $1 in
         pub)
             public_list "$2"
-            shift
             ;;
         *)
             echo "Wrong: $@"
             echo "Usage: [type] [list]"
+            ;;
     esac
+    shift
 done
